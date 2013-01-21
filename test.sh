8de1d4ebe09f38f91313b1a2acd2f9c36a18b549
Evan Altemose <evan18045@gmail.com>
2013-01-21 17:40:41 -0500

    testing the commit command

---
+++ test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#===============================================================================
+#
+#          FILE:  test.sh
+# 
+#         USAGE:  ./test.sh 
+# 
+#   DESCRIPTION:  testing git
+# 
+#       OPTIONS:  ---
+#  REQUIREMENTS:  ---
+#          BUGS:  ---
+#         NOTES:  ---
+#        AUTHOR:   (), 
+#       COMPANY:  
+#       VERSION:  1.0
+#       CREATED:  01/21/2013 05:39:17 PM EST
+#      REVISION:  ---
+#===============================================================================
+
+
